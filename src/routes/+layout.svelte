7da74918201cb4ce086942eba39afe29a2413996
Roman Kolesnev <roman.kolesnev.v@gmail.com>
2026-02-18 23:24:03 +0100

    feat: add Mastodon meta to header
    It allows better integration with fediverse.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,6 +7,8 @@
 
 <svelte:head>
   <link rel="icon" href={favicon} />
+  <link rel="me" href="https://mastodon.social/@ffloyd" />
+  <meta name="fediverse:creator" content="@ffloyd@mastodon.social" />
 </svelte:head>
 
 {@render children()}
